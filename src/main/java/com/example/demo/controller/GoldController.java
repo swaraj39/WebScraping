4c0622172e1dfb5868c541866a34075be66826e3
swaraj39 <162737985+swaraj39@users.noreply.github.com>
2026-02-12 13:54:27 +0530

    Update GoldController.java

--- src/main/java/com/example/demo/controller/GoldController.java
+++ src/main/java/com/example/demo/controller/GoldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "https://web-scraping-front.vercel.app/")
+@CrossOrigin(origins = "https://web-scraping-front.vercel.app")
 @RestController
 public class GoldController {
 
